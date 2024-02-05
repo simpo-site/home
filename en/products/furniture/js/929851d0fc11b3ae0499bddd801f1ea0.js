c9f10122734f2f091eb7be8dff2dc14a51f3aeed
clients-shipanjodder <clients@shipanjodder.site>
2024-02-05 15:00:40 +0600

    feb05

---
+++ en/products/furniture/js/929851d0fc11b3ae0499bddd801f1ea0.js
@@ -0,0 +1,3 @@
+(function($){'use strict';let JetWooBuilderCustomIcon={init:function(){let widgets={'jet-single-add-to-cart.default':JetWooBuilderCustomIcon.widgetSingleAddToCart,};$.each(widgets,function(widget,callback){window.elementorFrontend.hooks.addAction('frontend/element_ready/'+widget,callback)})},widgetSingleAddToCart:function($scope){let btnWrapper=$scope.find('.jet-woo-btn-with-custom-icon-wrapper');if(btnWrapper.length>0){let content=$(btnWrapper).data('content');$(btnWrapper).find('.single_add_to_cart_button').html(content)}}}
+$(window).on('elementor/frontend/init',JetWooBuilderCustomIcon.init)}(jQuery))
+;
